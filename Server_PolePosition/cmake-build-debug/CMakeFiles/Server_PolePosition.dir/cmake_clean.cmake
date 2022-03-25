604e9c034b8feb9eb8af8954b14a4c8d7cfb1dab
PabloNaranjo78 <pablonaranjo5@estudiantec.cr>
2022-03-25 11:54:17 -0600

    Se inician proyectos

---
+++ Server_PolePosition/cmake-build-debug/CMakeFiles/Server_PolePosition.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Server_PolePosition.dir/main.c.obj"
+  "Server_PolePosition.exe"
+  "Server_PolePosition.exe.manifest"
+  "Server_PolePosition.pdb"
+  "libServer_PolePosition.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Server_PolePosition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
