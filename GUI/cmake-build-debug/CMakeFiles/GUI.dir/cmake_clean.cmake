38b0f501982bcfa155517ce369fd018ad30f3a4f
PabloNaranjo78 <pablonaranjo5@estudiantec.cr>
2022-03-29 12:52:15 -0600

    Se actualiza proyecto

---
+++ GUI/cmake-build-debug/CMakeFiles/GUI.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GUI.dir/main.c.obj"
+  "GUI.exe"
+  "GUI.exe.manifest"
+  "GUI.pdb"
+  "libGUI.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GUI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
