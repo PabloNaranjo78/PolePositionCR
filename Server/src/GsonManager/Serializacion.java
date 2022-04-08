1878cee752d68f873215d94cc1b6abe6150ddb19
JanM <jmarschatz@gmail.com>
2022-04-08 14:44:21 -0600

    Se elimina una clase que era de ejemplo

--- Server/src/GsonManager/Serializacion.java
+++ Server/src/GsonManager/Serializacion.java
@@ -6,35 +6,13 @@ import java.io.FileReader;
 
 public class Serializacion {
     public static void main(String[] args) {
-        Persona jan = new Persona("jan","Aguilar",21,118040303);
-
-        Gson gson = new Gson();
-        String json = gson.toJson(jan);
-        System.out.println(json);
-
-        Persona jan2 = gson.fromJson(json,Persona.class);
-        jan2.printPersona();
+//        Persona jan = new Persona("jan","Aguilar",21,118040303);
+//
+//        Gson gson = new Gson();
+//        String json = gson.toJson(jan);
+//        System.out.println(json);
+//        Persona jan2 = gson.fromJson(json,Persona.class);
+//        jan2.printPersona();
     }
 }
 
-class Persona {
-    private String nombre;
-    private String apellido;
-    private int edad;
-    private int cedula;
-
-    Persona(String nombre,String apellido, int edad,int cedula){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.edad = edad;
-        this.cedula = cedula;
-
-    }
-
-    public void printPersona(){
-        System.out.println("Hola me llamo "+this.nombre+" "+this.apellido+", tengo "+this.edad+" anos y mi cedula es "+
-                this.cedula);
-
-    }
-
-}
